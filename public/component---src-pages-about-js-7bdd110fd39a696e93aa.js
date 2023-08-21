2bf157904415d8c147ab28649669e1643042adff
Karen Kipchirchir <jeronokaren17@gmail.com>
2023-08-21 14:43:00 +0300

    Build update

---
+++ public/component---src-pages-about-js-7bdd110fd39a696e93aa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[682],{5465:function(e,t,a){a.r(t);var l=a(7294),n=a(1883);t.default=e=>{let{data:t}=e;return l.createElement(l.Fragment,null,l.createElement("h2",null,t.site.siteMetadata.title),l.createElement("div",null,t.site.siteMetadata.description),l.createElement(n.Link,{to:"/"},"Back to home"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-7bdd110fd39a696e93aa.js.map
